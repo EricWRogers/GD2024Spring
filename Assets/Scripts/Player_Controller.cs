6c47ef6c9b021bc4b6daea6bd77382a7891b1e0e
Stalkishpotato <jdmiller3134@gmail.com>
2024-04-20 11:23:34 -0500

    Setup code for determining if encounter
    This code takes place in the player-controller script

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public LayerMask EncounterLayer;
+    const float baseThreshold = 1;
+    public float currentThreshold = baseThreshold;
 
     // Update is called once per frame
     void Update()
     {
-        
+        RollEncounter();
     }
+
+
+
+    public void RollEncounter()
+    {
+        if (Physics2D.OverlapCircle(transform.position, 0.2f, EncounterLayer)!= null || Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            int chance = UnityEngine.Random.Range(1, 101);
+            if (chance < currentThreshold)
+            {
+                Debug.Log("Encounter!");
+                currentThreshold = baseThreshold;
+            }
+            else
+            {
+                currentThreshold += 1;
+            }
+        }
+    }
+
 }
