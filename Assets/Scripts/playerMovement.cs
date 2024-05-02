2216db4af230499cd460da0ee6ed7fc6059659cd
DylanHilliard <ghilliard69420@gmail.com>
2024-05-02 09:06:34 -0500

    i dont remember all just pushing before class

--- Assets/Scripts/playerMovement.cs
+++ Assets/Scripts/playerMovement.cs
@@ -6,6 +6,7 @@ public class playerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
 
+    public Animator animator;
     public Rigidbody2D rb;
 
     Vector2 movement;
@@ -14,6 +15,10 @@ public class playerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
 
     void FixedUpdate()
