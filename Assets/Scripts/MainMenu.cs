992a5cf786900cd58dc0c5a0104640615518509c
DylanHilliard <ghilliard69420@gmail.com>
2024-05-03 17:29:41 -0500

    Main Menu works kinda
    have to change what scene play changes to in the script

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
 
     public void PlayGame ()
     {
- 
+        Debug.Log("Play");
+        SceneManager.LoadSceneAsync(1) ;
     }
 
     public void QuitGame()
